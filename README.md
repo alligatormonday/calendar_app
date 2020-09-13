94c1f31664269dd0fea90711f58a604028114be8
alligatormonday <joey@joeyjepson.com>
2020-09-12 21:45:47 -0700

    Formatted code within JS, HTML. Cleaned up unecessary code. Created and finished README.md

--- README.md
+++ README.md
@@ -1,1 +1,33 @@
 # calendar_app
+
+## Author: alligatormonday (Joseph Jepson)
+
+## Description 
+
+This is my submission for my fifth homework assignment for the University of Arizona - Coding Bootcamp for Full-Stack Web Development program.
+
+In this assignment I was tasked with creating a daily planner/calendar application that does the following: 
+* Upon opening the application, the current day and time is displayed at the top of the calendar
+* When viewing the timeblocks for the day, each timeblock is color coded to represent the past, present, and future
+* User can click within the event for a given timeblock and type an event with saving capabilities
+* Text for that event is saved in local storage
+* When page is reloaded, then the saved event persists
+
+![Image of calendar application](/images/screenshot.png)
+
+I used pre-existing HTML and CSS (provided by the instructor) and modified it with Bootstrap. I coded the JavaScript from scratch and implemented _Moment.js 2.27.0_.
+
+View deployment here: [calendar_app](https://alligatormonday.github.io/calendar_app/)
+
+My main goal with this assignment was to gain a stronger understanding of _local storage_. I kept things simple. There's nothing special or unique going on in this code. But I'm really proud of it's simplicity. This is the first assignment that I probably spent the least amount of time on, but not to the detriment of the assignment. 
+
+Understanding _moment.js_ is still something I do not feel super comfortable with, yet. However, this was the first time I've seen it, or used it. 
+
+Overall, this was a fun assignment that was definitely challenging but it made a lot of sense and wasn't too difficult. 
+
+## Credits
+
+* [Andrew Moses](https://github.com/andrewmosesdrive) - helped me with starting moment.js implementation
+* [Brandon Burrus](https://github.com/BrandonBurrus) - helped me with current day functionality
+* [Anthony Demott](https://github.com/java152001) - helped me with understanding local.storage
+* [John Hernandez](https://github.com/jdhern17) - helped me get off to a strong start with my _.on ("click")_ save function 
--- script.js
+++ script.js
@@ -1,109 +1,65 @@
 $(document).ready(function () {
-// console.log("document loaded")
+  // console.log("document loaded")
 
-// Variable
-let saveEvent = "";
+  //Renders current day and time in header   
+  $("#currentDay").text(moment().format("MMMM Do YYYY, H:mm"));
 
-$("#currentDay").text(moment().format("MMMM Do YYYY, h:mm:ss a"));
+  // Variables
+  let saveEvent = "";
 
   let currentTime = moment().hour();
 
-// Time Block
-$(".time-text").each(function(){
+  // Time Block 
+  $(".time-text").each(function () {
     let timeText = $(this).data("time");
     // console.log(timeText)
-    
-})
+  });
 
-// Local Storage
-$(".calendar-text").each(function (){
+  // Local Storage
+  $(".calendar-text").each(function () {
     // console.log($(this))
-    let eleId = $(this).attr("id")
+    let eleId = $(this).attr("id");
     // console.log(eleId)
-    let localVal = localStorage.getItem(eleId)
+    let localVal = localStorage.getItem(eleId);
     // console.log(localVal)
-    if (localVal !== null){
-        $(this).val(localVal)
+    if (localVal !== null) {
+      $(this).val(localVal);
     }
 
     currentTime = moment().hour();
     // console.log(currentTime)
-     let timeBlock = $(this).attr("data-hour");
-     if (timeBlock > currentTime){
-         $(this).addClass("future");
-     } else if (timeBlock < currentTime){
-         $(this).addClass("past");
-     } else if (timeBlock == currentTime){
-         $(this).addClass("present");
-     }
-});
+    let timeBlock = $(this).attr("data-hour");
+    if (timeBlock > currentTime) {
+      $(this).addClass("future");
+    } else if (timeBlock < currentTime) {
+      $(this).addClass("past");
+    } else if (timeBlock == currentTime) {
+      $(this).addClass("present");
+    }
+  });
 
-// console.log($(".calendar-text"))
+  // console.log($(".calendar-text"))
 
-// Event Listener for Calendar Event save button
-$(".save-event").on("click", function(){
+  // Event Listener for Calendar Event save button
+  $(".save-event").on("click", function () {
     // console.log("click")
     // Capture text-area input content
-    saveEvent = $(this).parent().siblings(".calendar-event").children(".calendar-text").val();
+    saveEvent = $(this)
+      .parent()
+      .siblings(".calendar-event")
+      .children(".calendar-text")
+      .val();
     // console.log(saveEvent)
-    let eleId = $(this).parent().siblings(".calendar-event").children(".calendar-text").attr("id");
+    let eleId = $(this)
+      .parent()
+      .siblings(".calendar-event")
+      .children(".calendar-text")
+      .attr("id");
     // console.log(eleId)
-    localStorage.setItem(eleId, saveEvent)
+    localStorage.setItem(eleId, saveEvent);
+  });
 
-})
-
-$(".save-event").on("click", function(){
+  $(".save-event").on("click", function () {
     location.reload();
-})
-
-})
-
-
-
-
-// # 05 Third-Party APIs: Work Day Scheduler
-
-// Create a simple calendar application that allows the user to save events for each hour of the day. This app will run in the browser and feature dynamically updated HTML and CSS powered by jQuery.
-
-// You'll need to use the [Moment.js](https://momentjs.com/) library to work with date and time. Be sure to read the documentation carefully and concentrate on using Moment.js in the browser.
-
-// ## User Story
-
-// ```
-// AS AN employee with a busy schedule
-// I WANT to add important events to a daily planner
-// SO THAT I can manage my time effectively
-// ```
-
-// ## Acceptance Criteria
-
-// ```
-// GIVEN I am using a daily planner to create a schedule
-// WHEN I open the planner
-// THEN the current day is displayed at the top of the calendar
-// WHEN I scroll down
-// THEN I am presented with timeblocks for standard business hours
-// WHEN I view the timeblocks for that day
-// THEN each timeblock is color coded to indicate whether it is in the past, present, or future
-// WHEN I click into a timeblock
-// THEN I can enter an event
-// WHEN I click the save button for that timeblock
-// THEN the text for that event is saved in local storage
-// WHEN I refresh the page
-// THEN the saved events persist
-// ```
-
-// The following animation demonstrates the application functionality:
-
-// ![day planner demo](./Assets/05-third-party-apis-homework-demo.gif)
-
-// ## Review
-
-// You are required to submit the following for review:
-
-// * The URL of the deployed application.
-
-// * The URL of the GitHub repository. Give the repository a unique name and include a README describing the project.
-
-// - - -
-// © 2019 Trilogy Education Services, a 2U, Inc. brand. All Rights Reserved.
+  });
+});
